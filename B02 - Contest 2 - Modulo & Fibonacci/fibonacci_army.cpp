c396a69f2944bd282157ed49b8f7cc2d67767a8b
Taha El Amine Kassabi <taha.kassabi@gmail.com>
2023-05-15 15:12:56 +0200

    B - Contest - Modulo & Fibonacci

---
+++ B02 - Contest 2 - Modulo & Fibonacci/fibonacci_army.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define Speed                   \
+  ios_base::sync_with_stdio(0); \
+  cin.tie(0);                   \
+  cout.tie(0)
+#define print(i) cout << i << endl
+#define ll long long
+
+int fibonacci(int n) {
+    int fib[20];
+
+    fib[0] = 1;
+    fib[1] = 1;
+
+    for (int i = 2; i <= n; i++) {
+        fib[i] = fib[i - 1] + fib[i - 2];
+    }
+
+    return fib[n];
+}
+
+int main() {
+    Speed;
+
+    int n;
+
+    cin >> n;
+
+    print(fibonacci(n));
+
+    return 0;
+}
